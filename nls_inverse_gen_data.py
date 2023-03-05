94a828628dda4712f7f5003b2e88711da7710902
npofsi | N/P硅 <npofsi@outlook.com>
2023-03-05 21:59:23 +0800

    lambda1: 1.04039e+00, lambda2: 1.92043e+00

--- nls_inverse_gen_data.py
+++ nls_inverse_gen_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pyDOE import lhs
 import random
 
 k1=1.5
@@ -18,16 +19,16 @@ nff=nf.conjugate()
 ng=complex(ng1,-ng2)
 ngg=ng.conjugate()
 
-n=100
+n=200
 
 lines= []
 file=open('data_x_new.csv', 'w')
 file.write("x,t,r1,r2,r3,r4\n")
+X=lhs(1,samples=n)*20-10
 for v in range(n):
 
-    x=20*random.random()-10
+    x=float(X[v])
     t=0 #4*random.random()-2
-
     f=np.exp((0-1j)*(k**2)*t + k*x + nf)
     ff=f.conjugate()
     g=np.exp((0-1j)*(l**2)*t + l*x + ng)
@@ -60,12 +61,14 @@ file.write("\n".join(lines))
 filet=open('data_t_new.csv', 'w')
 filet.write("t,x1,x2\n")
 linest=[]
+T=lhs(1,samples=n)*4-2
 for v in range(n):
 
-    x1=10
-    x2=-10
+    x1=5*np.pi
+    x2=-5*np.pi
 
-    t=4*random.random()-2
+    t=float(T[v])
+
 
 
     line = "{},{},{}".format(t, x1, x2).replace("(","").replace(")","")
@@ -80,10 +83,13 @@ filet.write("\n".join(linest))
 filexx=open('data_x_x.csv', 'w')
 filexx.write("x,t,r1,r2,r3,r4\n")
 linesxx=[]
-for v in range(15000):
+index=8000
+X=lhs(1,samples=index)*20-10
+T=lhs(1,samples=index)*4-2
+for v in range(index):
 
-    x=20*random.random()-10
-    t=4*random.random()-2
+    x=float(X[v])
+    t=float(T[v])
 
     f=np.exp((0-1j)*(k**2)*t + k*x + nf)
     ff=f.conjugate()
@@ -107,6 +113,7 @@ for v in range(15000):
     line = "{},{},{},{},{},{}".format(x, t, result1.real, result2.real, result3.real, result4.real).replace("(","").replace(")","")
 
     print(line)
+
     linesxx.append(line)
 
 
